7bc44eab3d035ab126ad66f3804f75faeb4531d4
laptop-Emax-1 <laptop-Emax-1@Laptop-Emax1>
2017-10-03 17:57:42 +0530

    test

---
+++ springbootlogin/src/main/java/com/springbootlogin/SpringbootloginApplication.java
@@ -0,0 +1,10 @@
+package com.springbootlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class SpringbootloginApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootloginApplication.class, args);
+	}
+}
